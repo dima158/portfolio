9fa61aadf8601625ff6e0e101f66306eb8daa4af
fabysshk <fabysshk@gmail.com>
2019-11-25 00:33:29 +0200

    base

--- src/index.js
+++ src/index.js
@@ -1,26 +1,4 @@
-import './stylesheets/main.scss';
-
+import './index.scss';
 import '@babel/polyfill';
 
-import {log, createEl, appendTo, text, css} from "./js/helpers";
-import {} from './js/functions'; // Empty
-import './js/packedges';
 
-import Interface from './js/classes/Interface';
-
-Interface.activate(); // Empty
-
-import {header} from "./js/components/header";
-
-const links = [
-    {
-        path: "/",
-        name: "home"
-    },
-    {
-        path: "/about",
-        name: "about as"
-    },
-];
-
-appendTo(document.body, header(links));
