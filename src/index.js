544dde5d56ee535f97b7ba4cee668db7ec6b1a07
Andrei Dudnik <andreidudnik6@gmail.com>
2019-11-27 23:03:28 +0200

    Replacement media query to mixins

--- src/index.js
+++ src/index.js
@@ -8,35 +8,35 @@ import './js/packedges';
 
 import Interface from './js/classes/Interface';
 
-let menuButton = document.querySelector('.mobile-nav-btn');
-let navMenu = document.querySelector('.nav');
-let menuOpen = false;
-
-window.addEventListener('resize', resize);
-menuButton.addEventListener('click', hamburger);
-
-function resize() {
-    let width = window.innerWidth;
-
-    if (width <= 768 && menuOpen === false) {
-        navMenu.classList.add('nav_hide');
-        menuButton.classList.remove('mobile-nav-btn_active');
-    } else if (width > 768 && menuOpen === true) {
-        navMenu.classList.remove('nav_hide');
-        menuButton.classList.remove('mobile-nav-btn_active');
-        menuOpen = false;
-    } else {
-        navMenu.classList.remove('nav_hide');
-    }
-}
-
-function hamburger() {
-    this.classList.toggle('mobile-nav-btn_active');
-    navMenu.classList.toggle('nav_hide');
-
-    menuOpen = navMenu.classList.contains('nav_hide') ? false : true;
-}
-
-resize();
+// let menuButton = document.querySelector('.mobile-nav-btn');
+// let navMenu = document.querySelector('.nav');
+// let menuOpen = false;
+//
+// window.addEventListener('resize', resize);
+// menuButton.addEventListener('click', hamburger);
+//
+// function resize() {
+//     let width = window.innerWidth;
+//
+//     if (width <= 768 && menuOpen === false) {
+//         navMenu.classList.add('nav_hide');
+//         menuButton.classList.remove('mobile-nav-btn_active');
+//     } else if (width > 768 && menuOpen === true) {
+//         navMenu.classList.remove('nav_hide');
+//         menuButton.classList.remove('mobile-nav-btn_active');
+//         menuOpen = false;
+//     } else {
+//         navMenu.classList.remove('nav_hide');
+//     }
+// }
+//
+// function hamburger() {
+//     this.classList.toggle('mobile-nav-btn_active');
+//     navMenu.classList.toggle('nav_hide');
+//
+//     menuOpen = navMenu.classList.contains('nav_hide') ? false : true;
+// }
+//
+// resize();
 
 Interface.activate(); // Empty
