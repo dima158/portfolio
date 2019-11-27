4e0d36bfc36f0dda6d3368276e6dabed295f11b7
Andrei Dudnik <andreidudnik6@gmail.com>
2019-11-27 17:29:31 +0200

    Section 'first' -> add media styles and hamburger menu

--- src/index.js
+++ src/index.js
@@ -8,5 +8,35 @@ import './js/packedges';
 
 import Interface from './js/classes/Interface';
 
+let menuButton = document.getElementsByClassName('mobile-nav-btn')[0];
+let navMenu = document.getElementsByClassName('nav')[0];
+let menuOpen = false;
+
+window.addEventListener('resize', resize);
+menuButton.addEventListener('click', hamburger);
+
+function resize() {
+    let width = window.innerWidth;
+
+    if (width <= 768 && menuOpen === false) {
+        navMenu.classList.add('nav_hide');
+        menuButton.classList.remove('mobile-nav-btn_active');
+    } else if (width > 768 && menuOpen === true) {
+        navMenu.classList.remove('nav_hide');
+        menuButton.classList.remove('mobile-nav-btn_active');
+        menuOpen = false;
+    } else {
+        navMenu.classList.remove('nav_hide');
+    }
+}
+
+function hamburger() {
+    this.classList.toggle('mobile-nav-btn_active');
+    navMenu.classList.toggle('nav_hide');
+
+    menuOpen = navMenu.classList.contains('nav_hide') ? false : true;
+}
+
+resize();
 
 Interface.activate(); // Empty
