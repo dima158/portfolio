384d9ec07816e94aad83189160883f55ceddecbb
Andrei Dudnik <andreidudnik6@gmail.com>
2019-12-06 13:07:47 +0200

    Refactor js, replace font-awesome -> iconmoon

--- src/index.js
+++ src/index.js
@@ -13,143 +13,145 @@ const menuButton = document.querySelector('.burger');
 const navMenu = document.querySelector('.nav');
 const menuLink = navMenu.querySelectorAll('.nav__link');
 
-window.addEventListener('resize', resize);
-menuButton.addEventListener('click', burgerMenuToggle);
-[...menuLink].forEach((current) => { current.addEventListener('click', link); });
-
-function resize() {
-    if (innerWidth > 768) burgerMenuClose();
-}
-
 function burgerMenuToggle() {
-    menuButton.classList.toggle('burger_active');
-    navMenu.classList.toggle('nav_show');
+  menuButton.classList.toggle('burger_active');
+  navMenu.classList.toggle('nav_show');
 }
 
 function burgerMenuClose() {
-    menuButton.classList.remove('burger_active');
-    navMenu.classList.remove('nav_show');
+  menuButton.classList.remove('burger_active');
+  navMenu.classList.remove('nav_show');
+}
+
+function resize() {
+  if (window.innerWidth > 768) burgerMenuClose();
 }
 
 function link(e) {
-    e.preventDefault();
-    burgerMenuClose();
-    //Links code
+  e.preventDefault();
+  burgerMenuClose();
+  //Links code
 }
+
+window.addEventListener('resize', resize);
+menuButton.addEventListener('click', burgerMenuToggle);
+[...menuLink].forEach(current => {
+  current.addEventListener('click', link);
+});
 
 // -----Projects Section-----
 const projectsButton = document.querySelectorAll('.projects__filter-item');
 const projects = document.querySelectorAll('.projects__project');
 
-[...projectsButton].forEach(function (element) {
-    element.addEventListener('click', portfolio);
-});
+function portfolioButtonActive(element) {
+  [...projectsButton].forEach(current => {
+    if (current === element) {
+      current.classList.add('projects__filter-item_active');
+    } else {
+      current.classList.remove('projects__filter-item_active');
+    }
+  });
+}
 
 function portfolio(e) {
-    portfolioButtonActive(e.target);
-    if (e.target.dataset.cat === 'all') {
-        [...projects].forEach(function (element) {
-            element.classList.remove('projects__project_hide');
-        });
-    } else {
-        [...projects].forEach(function (element) {
-            if (element.dataset.cat === e.target.dataset.cat) {
-                element.classList.remove('projects__project_hide');
-            } else {
-                element.classList.add('projects__project_hide');
-            }
-        });
-    }
+  portfolioButtonActive(e.target);
+  if (e.target.dataset.cat === 'all') {
+    [...projects].forEach(element => {
+      element.classList.remove('projects__project_hide');
+    });
+  } else {
+    [...projects].forEach(element => {
+      if (element.dataset.cat === e.target.dataset.cat) {
+        element.classList.remove('projects__project_hide');
+      } else {
+        element.classList.add('projects__project_hide');
+      }
+    });
+  }
 }
 
-function portfolioButtonActive(element) {
-    [...projectsButton].forEach(function (current) {
-        if (current === element) {
-            current.classList.add('projects__filter-item_active');
-        } else {
-            current.classList.remove('projects__filter-item_active');
-        }
-    });
-}
+[...projectsButton].forEach(element => {
+  element.addEventListener('click', portfolio);
+});
 
 // -----Slider-----
 const slider = document.querySelector('.members__list');
 const sliderItemsCount = slider.querySelectorAll('.members__person').length;
 const sliderDots = document.querySelector('.members__slider-dots');
+const sliderBtns = [];
 let sliderItemWidth = slider.querySelector('.members__person').offsetWidth;
 let sliderBtnCount = Math.ceil(sliderItemsCount / (slider.offsetWidth / sliderItemWidth));
-let sliderBtn = [];
-
-sliderAddDots();
 
 function sliderMove(offset) {
-    let sliderLeft = slider.offsetWidth * +offset;
-    slider.style.transform = 'translateX(' + -sliderLeft + 'px)';
+  const sliderLeft = slider.offsetWidth * +offset;
+  slider.style.transform = `translateX(${-sliderLeft}px)`;
 }
 
 function sliderDotsActive(event) {
-    [...sliderBtn].forEach((current) => {
-        if (current === event.target) {
-            current.classList.add('members__slider-btn_active');
-        } else {
-            current.classList.remove('members__slider-btn_active');
-        }
-    });
-    sliderMove(event.target.dataset.slider);
+  [...sliderBtns].forEach(current => {
+    if (current === event.target) {
+      current.classList.add('members__slider-btn_active');
+    } else {
+      current.classList.remove('members__slider-btn_active');
+    }
+  });
+  sliderMove(event.target.dataset.slider);
 }
 
 function sliderAddDots() {
-    sliderBtn.length = 0;
-    sliderDots.innerHTML = '';
-    for (let i = 0; i < sliderBtnCount; i++) {
-        let btn = document.createElement('button');
-        btn.classList.add('members__slider-btn');
-        if (i === 0) btn.classList.add('members__slider-btn_active');
-        btn.dataset.slider = i;
-        sliderDots.append(btn);
-        btn.addEventListener('click', sliderDotsActive);
-        sliderBtn.push(btn);
-    }
+  sliderBtns.length = 0;
+  sliderDots.innerHTML = '';
+  for (let i = 0; i < sliderBtnCount; i++) {
+    const btn = document.createElement('button');
+    btn.classList.add('members__slider-btn');
+    if (i === 0) btn.classList.add('members__slider-btn_active');
+    btn.dataset.slider = i;
+    sliderDots.append(btn);
+    btn.addEventListener('click', sliderDotsActive);
+    sliderBtns.push(btn);
+  }
 }
 
 function resizeSlider() {
-    let currentOffset = document.querySelector('.members__slider-btn_active').dataset.slider;
-    let itemWidth = slider.querySelector('.members__person').offsetWidth;
-    let btnCount = Math.ceil(sliderItemsCount / (slider.offsetWidth / itemWidth));
+  const currentOffset = document.querySelector('.members__slider-btn_active').dataset.slider;
+  const itemWidth = slider.querySelector('.members__person').offsetWidth;
+  const btnCount = Math.ceil(sliderItemsCount / (slider.offsetWidth / itemWidth));
 
-    sliderItemWidth = itemWidth;
-    if (btnCount !== sliderBtnCount) {
-        slider.style.transform = 'translateX(0)';
-        sliderBtnCount = btnCount;
-        sliderAddDots();
-    } else {
-        sliderMove(currentOffset);
-    }
+  sliderItemWidth = itemWidth;
+  if (btnCount !== sliderBtnCount) {
+    slider.style.transform = 'translateX(0)';
+    sliderBtnCount = btnCount;
+    sliderAddDots();
+  } else {
+    sliderMove(currentOffset);
+  }
 }
+
+sliderAddDots();
 
 // -----Resize-----
-window.addEventListener('resize', resizeEvent);
-
 function resizeEvent() {
-    resizeSlider();
+  resizeSlider();
 }
+
+window.addEventListener('resize', resizeEvent);
 
 // -----Instagram Photo-----
 async function getPhoto() {
-    const article = document.querySelectorAll('.article');
-    const data = await fetch('https://www.instagram.com/website_of_the_day/?__a=1')
-        .then(response => response.json())
-        .then(data => data.graphql.user.edge_owner_to_timeline_media.edges);
+  const article = document.querySelectorAll('.article');
+  const photos = await fetch('https://www.instagram.com/website_of_the_day/?__a=1')
+    .then(response => response.json())
+    .then(data => data.graphql.user.edge_owner_to_timeline_media.edges);
 
-    for (let i = 0; i < 3; i++) {
-        const image = article[i].querySelector('.article__img');
-        let img = document.createElement('img');
-        img.src = data[i].node.thumbnail_src;
-        img.classList.add('article__background');
-        image.append(img);
-    }
+  for (let i = 0; i < 3; i++) {
+    const image = article[i].querySelector('.article__img');
+    const img = document.createElement('img');
+    img.src = photos[i].node.thumbnail_src;
+    img.classList.add('article__background');
+    image.append(img);
+  }
 }
-getPhoto();
 
+getPhoto();
 
 Interface.activate(); // Empty
